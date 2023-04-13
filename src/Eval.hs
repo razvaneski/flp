6d6729ae8d64c3489417e886aae80df62ed68ee0
Razvan Dumitriu <razvan.dumitriu@mready.net>
2023-04-13 12:06:28 +0300

    initial commit

---
+++ src/Eval.hs
@@ -0,0 +1,100 @@
+
+module Eval where
+
+import Exp
+import Data.List ( union, delete )
+
+vars :: Exp -> [IndexedVar]
+vars (X x) = [x]
+vars (Lam x e) = [x] `union` vars e
+vars (App e1 e2) = vars e1 `union` vars e2
+
+-- >>> vars (Lam (makeIndexedVar "x") (X (makeIndexedVar "y")))
+-- [IndexedVar {ivName = "x", ivCount = 0},IndexedVar {ivName = "y", ivCount = 0}]
+
+-- >>> vars (App (Lam (IndexedVar {ivName = "x", ivCount = 0}) (X (IndexedVar {ivName = "y", ivCount = 0}))) (X (IndexedVar {ivName = "z", ivCount = 0})))
+-- [IndexedVar {ivName = "x", ivCount = 0},IndexedVar {ivName = "y", ivCount = 0},IndexedVar {ivName = "z", ivCount = 0}]
+
+-- >>> vars (App (Lam (IndexedVar {ivName = "x", ivCount = 0}) (X (IndexedVar {ivName = "x", ivCount = 0}))) (X (IndexedVar {ivName = "x", ivCount = 0})))
+-- [IndexedVar {ivName = "x", ivCount = 0}]
+
+freeVars :: Exp -> [IndexedVar]
+freeVars = do
+    let v (X x) = [x]
+        v (Lam x e) = delete x (v e)
+        v (App e1 e2) = v e1 `union` v e2
+    v
+
+-- >>> freeVars (Lam (makeIndexedVar "x") (X (makeIndexedVar "y")))
+-- [IndexedVar {ivName = "y", ivCount = 0}]
+
+-- >>> freeVars  (App (Lam (IndexedVar {ivName = "x", ivCount = 0}) (X (IndexedVar {ivName = "y", ivCount = 0}))) (X (IndexedVar {ivName = "z", ivCount = 0})))
+-- [IndexedVar {ivName = "y", ivCount = 0},IndexedVar {ivName = "z", ivCount = 0}]
+
+-- >>> freeVars (App (Lam (IndexedVar {ivName = "x", ivCount = 0}) (X (IndexedVar {ivName = "x", ivCount = 0}))) (X (IndexedVar {ivName = "x", ivCount = 0})))
+-- [IndexedVar {ivName = "x", ivCount = 0}]
+
+-- >>> freeVars (Lam (IndexedVar {ivName = "x", ivCount = 0}) (App (X (IndexedVar {ivName = "x", ivCount = 0})) (X (IndexedVar {ivName = "x", ivCount = 0}))))
+-- []
+
+occursFree :: IndexedVar -> Exp -> Bool
+occursFree = do
+    let v x (X y) = x == y
+        v x (Lam y e) = x /= y && v x e
+        v x (App e1 e2) = v x e1 || v x e2
+    v
+
+-- >>> makeIndexedVar "x" `occursFree` Lam (makeIndexedVar "x") (X (makeIndexedVar "y"))
+-- False
+
+-- >>> makeIndexedVar "y" `occursFree` Lam (makeIndexedVar "x") (X (makeIndexedVar "y"))
+-- True
+
+freshVar :: IndexedVar -> [IndexedVar] -> IndexedVar
+freshVar = do
+    let v x xs = if x `elem` xs then v (x {ivCount = ivCount x + 1}) xs else x
+    v
+
+-- >>> freshVar (makeIndexedVar "x") [makeIndexedVar "x"]
+-- IndexedVar {ivName = "x", ivCount = 1}
+
+-- >>> freshVar (makeIndexedVar "x") [makeIndexedVar "x", IndexedVar {ivName = "x", ivCount = 1}, IndexedVar {ivName = "y", ivCount = 2}] 
+-- IndexedVar {ivName = "x", ivCount = 2}
+
+renameVar :: IndexedVar -> IndexedVar -> Exp -> Exp
+renameVar toReplace replacement = do
+    let v x y (X z) = if z == x then X y else X z
+        v x y (Lam z e) = if z == x then Lam y (v x y e) else Lam z (v x y e)
+        v x y (App e1 e2) = App (v x y e1) (v x y e2)
+    v toReplace replacement
+
+-- >>> renameVar (IndexedVar {ivName = "x", ivCount = 0}) (IndexedVar {ivName = "z", ivCount = 0}) (App (Lam (IndexedVar {ivName = "x", ivCount = 0}) (X (IndexedVar {ivName = "x", ivCount = 0}))) (X (IndexedVar {ivName = "x", ivCount = 0})))
+-- App (Lam (IndexedVar {ivName = "z", ivCount = 0}) (X (IndexedVar {ivName = "z", ivCount = 0}))) (X (IndexedVar {ivName = "z", ivCount = 0}))
+
+substitute :: IndexedVar -> Exp -> Exp -> Exp
+substitute toReplace replacement = do
+    let v x y (X z) = if z == x then y else X z
+        v x y (Lam z e) = if z == x then Lam z e else Lam z (v x y e)
+        v x y (App e1 e2) = App (v x y e1) (v x y e2)
+    v toReplace replacement
+
+-- >>> substitute (IndexedVar {ivName = "x", ivCount = 0}) (X (IndexedVar {ivName = "z", ivCount = 0})) (App (Lam (IndexedVar {ivName = "x", ivCount = 0}) (X (IndexedVar {ivName = "x", ivCount = 0}))) (X (IndexedVar {ivName = "x", ivCount = 0})))
+-- App (Lam (IndexedVar {ivName = "x", ivCount = 0}) (X (IndexedVar {ivName = "x", ivCount = 0}))) (X (IndexedVar {ivName = "z", ivCount = 0}))
+
+-- >>> substitute (IndexedVar {ivName = "y", ivCount = 0}) (X (IndexedVar {ivName = "x", ivCount = 0})) (App (Lam (IndexedVar {ivName = "x", ivCount = 0}) (X (IndexedVar {ivName = "y", ivCount = 0}))) (X (IndexedVar {ivName = "z", ivCount = 0})))
+-- App (Lam (IndexedVar {ivName = "x", ivCount = 1}) (X (IndexedVar {ivName = "x", ivCount = 0}))) (X (IndexedVar {ivName = "z", ivCount = 0}))
+
+normalize :: Exp -> Exp
+normalize x = do
+    let v (X x) = X x
+        v (Lam x e) = Lam x (v e)
+        v (App (Lam x e1) e2) = if x `occursFree` e2 then v (substitute x (v e2) e1) else v e1
+        v (App e1 e2) = App (v e1) (v e2)
+    v x
+
+-- >>> normalize (X (makeIndexedVar "x"))
+-- X (IndexedVar {ivName = "x", ivCount = 0})
+
+-- >>> normalize (App (Lam (IndexedVar {ivName = "y", ivCount = 0}) (X (IndexedVar {ivName = "x", ivCount = 0}))) (App (Lam (IndexedVar {ivName = "y", ivCount = 0}) (App (X (IndexedVar {ivName = "y", ivCount = 0})) (X (IndexedVar {ivName = "y", ivCount = 0})))) (Lam (IndexedVar {ivName = "y", ivCount = 0}) (App (X (IndexedVar {ivName = "y", ivCount = 0})) (X (IndexedVar {ivName = "y", ivCount = 0}))))))
+-- X (IndexedVar {ivName = "x", ivCount = 0})
+
